f33b92251ab528a1ae0f7a9ffe69cf37b172bdb2
abaileyb <bailey@baileyberro.com>
2015-07-01 14:21:03 +0200

    grades working for single student

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -9,9 +9,18 @@ class CoursesController < ApplicationController
 	end
 
 	def show
-		
+		@course = Course.find(params[:id])
+		@name_array = []
+		@id_array = getAssignmentIdsByCourseId(@course.cid, @name_array)
+
+		@grade_hash = getUsersGradesFromAssignmentsAndID(@id_array, 2667)
 	end
 
+
+
+
+
+	private
 	def getCourseIdsByUserId(id)
 		rest_data = RestClient.get 'https://testing.vle.getsmarter.co.za/webservice/rest/server.php?wstoken=0d1b35e7065c79172a800a0edf1cc080&wsfunction=core_enrol_get_users_courses&userid=10435&moodlewsrestformat=json'
 		json_data = JSON.load rest_data	
@@ -21,10 +30,58 @@ class CoursesController < ApplicationController
 			puts course['id']
 			empty_table[count][0] = course['id']
 			puts course['fullname']
-			empty_table[count][1] = course['name']
+			empty_table[count][1] = course['fullname']
 			count +=1
 		end
 		return empty_table
 	end
+
+	def getAssignmentIdsByCourseId(cid, namearray)
+		rest_data = RestClient.get "https://testing.vle.getsmarter.co.za/webservice/rest/server.php?wstoken=0d1b35e7065c79172a800a0edf1cc080&wsfunction=mod_assign_get_assignments&courseids[0]=#{cid}&moodlewsrestformat=json"
+		json_data = JSON.load rest_data	
+
+		puts json_data
+		assignments = json_data["courses"][0]["assignments"]
+
+		assignmentids = []
+
+		assignments.each do |assignment|
+			assignmentids << assignment["id"]
+			namearray << assignment["name"]
+		end
+
+	assignmentids #returns an array of assignment ids
+end
+
+
+	##TODO: REFACTOR THIS FUNCTION TO BE MUCH MORE EFFICENT
+	def getUsersGradesFromAssignmentsAndID(assignmentids, uid)
+		usersgrades = {}
+		assignmentids.each do |assignmentid|
+			rest_data = RestClient.get "https://testing.vle.getsmarter.co.za/webservice/rest/server.php?wstoken=0d1b35e7065c79172a800a0edf1cc080&wsfunction=mod_assign_get_grades&assignmentids[0]=#{assignmentid}&moodlewsrestformat=json"
+			json_data = JSON.load rest_data	
+
+			puts ''
+			puts ''
+			puts json_data
+
+			if (!json_data["assignments"].empty?)
+				grades = json_data["assignments"][0]["grades"]
+
+
+				grades.each do |grade|
+					if grade["userid"] == uid
+						usersgrades[assignmentid] = grade["grade"]
+					end
+				end
+			end
+
+		end
+
+		return usersgrades
+	end	
+
+
+
 
 end
