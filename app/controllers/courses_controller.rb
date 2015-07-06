b687570c94c148f91c1ebc8f44594830cbb18ae5
abaileyb <bailey@baileyberro.com>
2015-07-06 09:39:15 +0200

    needs to be styled

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
 	def index
-		courselist = getCourseIdsByUserId(10453)
+		courselist = getCourseIdsByUserId(2667)
 		Course.delete_all
 		courselist.each do |course|
 			Course.create(name: course[1], cid: course[0])
@@ -24,12 +24,12 @@ class CoursesController < ApplicationController
 	def getCourseIdsByUserId(id)
 		rest_data = RestClient.get 'https://testing.vle.getsmarter.co.za/webservice/rest/server.php?wstoken=0d1b35e7065c79172a800a0edf1cc080&wsfunction=core_enrol_get_users_courses&userid=10435&moodlewsrestformat=json'
 		json_data = JSON.load rest_data	
+		puts json_data
 		empty_table = Array.new(json_data.size) { Array.new(2) }
 		count = 0 
+		puts empty_table
 		json_data.each do |course|
-			puts course['id']
 			empty_table[count][0] = course['id']
-			puts course['fullname']
 			empty_table[count][1] = course['fullname']
 			count +=1
 		end
@@ -55,24 +55,70 @@ end
 
 
 	##TODO: REFACTOR THIS FUNCTION TO BE MUCH MORE EFFICENT
+	# def getUsersGradesFromAssignmentsAndID(assignmentids, uid)
+	# 	usersgrades = {}
+	# 	addurl = ""
+	# 	assignmentids.each do |assignmentid|
+
+
+	# 		rest_data = RestClient.get "https://testing.vle.getsmarter.co.za/webservice/rest/server.php?wstoken=0d1b35e7065c79172a800a0edf1cc080&wsfunction=mod_assign_get_grades&assignmentids[0]=#{assignmentid}&moodlewsrestformat=json"
+	# 		json_data = JSON.load rest_data	
+
+	# 		puts ''
+	# 		puts ''
+	# 		puts json_data
+
+	# 		if (!json_data["assignments"].empty?)
+	# 			grades = json_data["assignments"][0]["grades"]
+
+
+	# 			grades.each do |grade|
+	# 				if grade["userid"] == uid
+	# 					usersgrades[assignmentid] = grade["grade"]
+	# 				end
+	# 			end
+
+	# 		end
+
+	# 	end
+
+	# 	return usersgrades
+	# end	
+
+
+
 	def getUsersGradesFromAssignmentsAndID(assignmentids, uid)
 		usersgrades = {}
+
+		puts assignmentids
+
+		addurl = ""
+		count = 0
 		assignmentids.each do |assignmentid|
-			rest_data = RestClient.get "https://testing.vle.getsmarter.co.za/webservice/rest/server.php?wstoken=0d1b35e7065c79172a800a0edf1cc080&wsfunction=mod_assign_get_grades&assignmentids[0]=#{assignmentid}&moodlewsrestformat=json"
-			json_data = JSON.load rest_data	
+			addurl << "&assignmentids[#{count}]=#{assignmentid}"
+			count += 1
+		end
 
-			puts ''
-			puts ''
-			puts json_data
+		resturl = "https://testing.vle.getsmarter.co.za/webservice/rest/server.php?wstoken=0d1b35e7065c79172a800a0edf1cc080&wsfunction=mod_assign_get_grades&moodlewsrestformat=json" + addurl
+		rest_data = RestClient.get resturl
+		json_data = JSON.load rest_data	
 
-			if (!json_data["assignments"].empty?)
-				grades = json_data["assignments"][0]["grades"]
+		puts 'JSON STARTS HERE'
+		puts json_data
+
+		assignment_data = json_data["assignments"]
 
 
-				grades.each do |grade|
-					if grade["userid"] == uid
-						usersgrades[assignmentid] = grade["grade"]
-					end
+
+		assignment_data.each do |assignment|
+
+
+			assignmentid = assignment["assignmentid"]
+			grades = assignment["grades"]
+
+			grades.each do |grade|
+				if grade["userid"] == uid
+					usersgrades[assignmentid] = grade["grade"]
 				end
 			end
 
@@ -80,8 +126,6 @@ end
 
 		return usersgrades
 	end	
-
-
 
 
 end
--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,4 +1,11 @@
 module CoursesHelper
+	def gradeify(grade)
+		if grade
+			return (grade.chop!.chop!.chop!) + "%"
+		else
+			return ("No Grade")
+		end
+	end
 
 	
 end
